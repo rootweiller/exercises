906e63d1f3445c570a1e58d40b326e0592b9b9a4
rootweiller <rootweiller@MyloReloaded.(none)>
2015-02-25 15:20:17 -0430

    Condicionales con Python

---
+++ exercises1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Estas son variables
+x1=2
+x2=4
+r1=6
+r2=4
+
+
+#Comparando si x1 y x2 son iguales a x2 y r2
+if x1 and x2 == x2 and r2:
+	print "HOLA FUNCIONA"
+else:
+	print "Estas Loco"
