60905a85d47d88c6d33859b03fa42a7cae8a58d9
rootweiller <rootweiller@MyloReloaded.(none)>
2015-02-25 16:35:57 -0430

    Agregando variable y1 / y2 para el calculo

--- exercises1.py
+++ exercises1.py
@@ -5,10 +5,12 @@ x1=2
 x2=4
 r1=6
 r2=4
+y1=4
+y2=5
 
 
 #Comparando si x1 y x2 son iguales a x2 y r2
-if x1 and x2 == x2 and r2:
+if x1 or x1 or y1 == r1 or r2 or y2:
 	print "HOLA FUNCIONA"
 else:
 	print "Estas Loco"
